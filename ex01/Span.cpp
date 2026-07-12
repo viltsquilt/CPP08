eec7abc93f428ee55f8fa76cad08727c80edd871
Viljam Ahdekivi <ahdekivi.viljam@gmail.com>
2026-07-12 18:44:26 +0300

    ex01

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,11 +1,11 @@
 #include "Span.hpp"
 
-Span::Span()
+Span::Span() : _N(0)
 {
 
 }
 
-Span::Span(unsigned int N): _span(NULL), _N(N)
+Span::Span(unsigned int N): _N(N)
 {
 
 }
@@ -22,30 +22,30 @@ Span&	Span::operator=(const Span& orig)
 		_span = orig._span;
 		_N = orig._N;
 	}
-	return (*this)
+	return (*this);
 }
 
 void	Span::insertNumbers(std::vector<int> numbers)
 {
-	if ((_span.size() + numbers.size()) > N)
+	if ((_span.size() + numbers.size()) > _N)
 	{
 		throw std::out_of_range("not enough space!");
 	}
 	else
 	{
-		_span.insert(span.end(), numbers.begin(), numbers.end());
+		_span.insert(_span.end(), numbers.begin(), numbers.end());
 	}
 }
 
 void	Span::addNumber(int num)
 {
-	if (_span.size() < N)
+	if (_span.size() < _N)
 		_span.push_back(num);
 	else
 		throw std::out_of_range("not enough space!");
 }
 
-unsigned int	shortestSpan()
+unsigned int	Span::shortestSpan()
 {
 	if (_span.empty() || _span.size() == 1)
 		std::out_of_range("not enough numbers!");
@@ -60,12 +60,12 @@ unsigned int	shortestSpan()
 	return (shortest);
 }
 
-unsigned int	longestSpan()
+unsigned int	Span::longestSpan()
 {
 	if (_span.empty() || _span.size() == 1)
 		std::out_of_range("not enough numbers!");
 	sort(_span.begin(), _span.end());
-	return *(_span.end() - 1) - _span.begin();
+	return *(_span.end() - 1) - *_span.begin();
 }
 
 Span::~Span()
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,5 +2,26 @@
 
 int	main(void)
 {
-
+	try
+	{
+		int num = 10000;
+		Span s = Span(num + 1);
+		s.addNumber(-10);
+		Span s2(s);
+		for (int i = 0; i < num; i++)
+		{
+			s.addNumber(i);
+		}
+		std::cout << s.shortestSpan() << std::endl;
+		std::cout << s.longestSpan() << std::endl;
+		std::vector v = {220, 300, 1};
+		s2.insertNumbers(v);
+		std::cout << s2.shortestSpan() << std::endl;
+		std::cout << s2.longestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
 }
